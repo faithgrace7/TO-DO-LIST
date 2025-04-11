73bf888eed007f8d9ba5388de6d7201bbbc154a1
Faith Gutierrez <gutierrezfaithgrace7@gmail.com>
2025-04-11 08:56:44 +0800

    backend updated

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from .views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
-from rest_framework.authtoken.views import obtain_auth_token
-from myapp.views import MyProtectedView, SecureHelloView
 
 
 urlpatterns = [
@@ -9,10 +7,7 @@ urlpatterns = [
     path('create', TodoCreateView.as_view(), name='todo-create'),  # Create a new task
     path('<int:pk>/update', TodoUpdateView.as_view(), name='todo-update'),  # Update a specific task
     path('<int:pk>/delete', TodoDeleteView.as_view(), name='todo-delete'),  # Delete a specific task
-    path('api/protected/', MyProtectedView.as_view(), name='protected-view'),
-    path('secure-hello/', SecureHelloView.as_view(), name='secure-hello-view'),
-    path('api/token/', obtain_auth_token, name='token-obtain'),
-    path('api-token-auth/', obtain_auth_token),
+   
 
 
 ]
--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from myapp.views import MyProtectedView, SecureHelloView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/todos/', include('myapp.urls')), 
+    path('secure-hello/', SecureHelloView.as_view(), name='secure-hello-view'),
+    path('api/token/', obtain_auth_token, name='token-obtain'),
+    path('api-token-auth/', obtain_auth_token),
 ]
